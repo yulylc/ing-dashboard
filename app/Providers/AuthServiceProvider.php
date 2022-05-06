1b9eaf2bb1bab63104d055aea320cbaeb289481b
Yuly LC <ylopezco84@gmail.com>
2022-05-06 15:38:42 -0400

    Creado el user superadmin

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -24,7 +24,10 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        //
+        
+        Gate::before(function ($user, $ability){
+            return $user->email == 'admin@gmail.com' ?? null;
+        });
+        
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,7 @@
                     @endif
                     {{ __('You are logged in!') }} --}}
                     {{-- Mi codigo comienza aqui --}}
+                    <a class="btn btn-warning" href="{{ route('users.create')}}">Nuevo</a>
                 </div>
             </div>
         </div>
