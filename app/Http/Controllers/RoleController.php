8086b42b812b2018f8cdafacbfb4f3a084c8bc29
Yuly LC <ylopezco84@gmail.com>
2022-05-06 14:53:53 -0400

    Configurados los controladores User y Roles

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Symfony\Contracts\Service\Attribute\Required;
+
+class RoleController extends Controller
+{
+    public function __construct()
+    {
+        // $this->middleware('permission:ver-rol|crear-rol|editar-rol|eliminar-rol', ['only'=>['index']]);
+        // $this->middleware('permission:crear-rol', ['only'=>['create','store']]);
+        // $this->middleware('permission:editar-rol', ['only'=>['edit','update']]);
+        // $this->middleware('permission:eliminar-rol', ['only'=>['destroy']]);
+
+        $this->middleware('permission:ver-rol|crear-rol|editar-rol|eliminar-rol')->only('index');
+        $this->middleware('permission:crear-rol')->only('create','store');
+        $this->middleware('permission:editar-rol')->only('edit','update');
+        $this->middleware('permission:eliminar-rol')->only('destroy');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+      * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $roles = Role::paginate(5);
+        return view('roles.index',compact('roles'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $permission = Permission::get();
+        return view('roles.crear', compact('permission'));    
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, ['name'=>'required', 'permission'=>'required']);
+        $roles = Role::create(['name'=>$request->input('name')]);
+        $roles->syncPermissions($request->input('permission'));
+        
+        return redirect()->route('roles.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $role = Role::find($id);
+        $permission = Permission::get();
+        $rolePermissions = DB::table('role_has_permissions')->where('role_has_permissions.role_id', $id)
+            ->pluck('role_has_permissions.permission_id', 'role_has_permissions.permission_id')
+            ->all();
+
+        return view('roles.editar', compact('role','permission','rolePermissions'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, ['name'=>'required', 'permission'=>'required']);
+        
+        $role = Role::find($id);
+        $role->name = $request->input('name');
+        $role->save();
+
+        $role->syncPermissions($request->input('permission'));
+        
+        return redirect()->route('roles.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        DB::table('roles')->where('id',$id)->delete();
+        return redirect()->route('roles.index');
+    }
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Spatie\Permission\Models\Role;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Arr;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $usuarios = User::paginate(5); //revisar luego para ordenar
+        return view('users.index', compact('usuarios'));
+           
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $roles = Role::pluck('name','name')->all();
+        return view('users.crear', compact('roles'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name'=>'required',
+            'email'=>'required|email|unique:users,email',
+            'password'=>'required|same:confirm-password',
+            'roles'=>'required',
+        ]);
+
+        $input = $request->all();
+        $input['password'] = Hash::make($input('password'));
+
+        $user = User::create($input);
+        $user->assignRole($request->input('roles'));
+
+        return redirect()->route('users.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = User::find($id);
+        $roles = Role::pluck('name','name')->all();
+        $userRole = $user->roles->pluck('name','name')->ftp_all();
+
+        return view('users.editar', compact('user','roles','userRole'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'name'=>'required',
+            'email'=>'required|email|unique:users,email'.$id,
+            'password'=>'same:confirm-password',
+            'roles'=>'required',
+        ]);
+
+        $input = $request->all();
+        if (!empty($input['password'])){
+            $input['password'] = Hash::make($input['password']);
+        }else{
+            $input = Arr::except($input, array('password'));
+        };
+
+        $user = User::find($id);
+        $user->update($input);
+        DB::table('model_has_roles')->where('model_id', $id)->delete();
+
+        $user->assignRole($request->input('roles'));
+        return redirect()->route('users.index');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        User::find($id)->delete();
+        return redirect()->route('users.index');
+    }
+}
